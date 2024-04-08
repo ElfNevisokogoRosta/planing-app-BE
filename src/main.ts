604b068a43192fdab39af4bc17b944c1847f6c2b
ElfNevisokogoRosta <anatoliy.tymoshenko.work@gmail.com>
2024-04-08 22:23:11 +0300

    hostname update

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 require('dotenv').config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.BASE_PORT || 3000);
+  await app.listen(process.env.BASE_PORT || 3000, '0.0.0.0');
 }
 bootstrap();
