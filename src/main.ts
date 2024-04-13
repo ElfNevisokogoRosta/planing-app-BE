8bce32def9f6cb70d6a4a61512d51fe05a10eeab
ElfNevisokogoRosta <anatoliy.tymoshenko.work@gmail.com>
2024-04-13 17:47:21 +0300

    added api prefix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config();
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  const corsOptions: CorsOptions = {
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  app.enableCors(corsOptions);
+
   await app.listen(process.env.BASE_PORT || 3000, '0.0.0.0');
 }
 bootstrap();
